7339a01a0b02eb09b7dc2d96e295856df0d46773
alejandroolmed59 <00097017@uca.edu.sv>
2019-05-31 09:41:12 -0600

    Subiendo

---
+++ app/src/main/java/com/petrlr14/mvvm/database/repositories/GitHubRepoRepository.kt
@@ -0,0 +1,33 @@
+package com.petrlr14.mvvm.database.repositories
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import com.petrlr14.mvvm.database.daos.GitHubDAO
+import com.petrlr14.mvvm.services.retrofit.GithubService
+import com.petrlr14.mvvm.database.entities.GitHubRepo
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class GitHubRepoRepository (private val repoDao:GitHubDAO){
+
+    fun retrieveReposAsync(user:String):Deferred<Response<List<GitHubRepo>>> =
+        GithubService.getGithubService().getAllReposPerUser(user)
+
+    @WorkerThread
+    suspend fun insert(repo:GitHubRepo){
+        repoDao.insert(repo)
+    }
+
+    fun getAll():LiveData<List<GitHubRepo>>{
+        return repoDao.getAllRepos()
+    }
+
+    @WorkerThread
+    suspend fun nuke(){
+        return repoDao.nukeTable()
+    }
+
+}
